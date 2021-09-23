30fb9167c7a7e1e6b7332240572e2e6873911919
AzureAD\NashmilSepehriazad <nse@leapwork.com>
2021-09-23 18:49:27 +0200

    created the route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import First from "./components/First";
+import Second from "./components/Second";
+import Third from "./components/Third";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <First />
+      <Second />
+      <Third />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/First.tsx
@@ -0,0 +1,3 @@
+export default function First() {
+  return <div>First Content</div>;
+}
