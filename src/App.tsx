583d5bfc2e6c1392f7b9fee23b2bcb8408f02b24
AzureAD\NashmilSepehriazad <nse@leapwork.com>
2021-09-23 19:01:37 +0200

    implemented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import First from "./components/First";
 import Second from "./components/Second";
 import Third from "./components/Third";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <First />
-      <Second />
-      <Third />
+      <Switch>
+        <Route path="/poc/1">
+          <First />
+        </Route>
+        <Route path="/poc/2">
+          <Second />
+          <Third />
+        </Route>
+        <Route path="/poc/3">
+          <First />
+          <Third />
+        </Route>
+      </Switch>
     </div>
   );
 }
